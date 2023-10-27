40299c1cba870fd5a777ab53e5a7a86cee0453c8
Menyhart Laszlo <laszlo.g.menyhart@gmail.com>
2023-10-27 17:05:31 +0200

    init cs

---
+++ cs/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace play
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Blackjack");
+
+            /// declaration
+            /// 
+
+            /// read input data
+            /// 
+
+            /// solve the problem - implement algorithms
+            /// 
+
+            /// write out answers
+            /// 
+
+
+        }
+    }
+}
